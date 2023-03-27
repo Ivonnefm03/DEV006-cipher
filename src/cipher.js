0fda8f6a730141cc48ecd2c7a966eec2a5fbe184
Ivonne Figueredo <ivonnefm03@gmail.com>
2023-03-27 11:12:31 -0500

    decode terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,70 @@
 const cipher = {
-  // ...
+
+  encode: function (message, displacement){ 
+    // Verifico si el mensaje es nulo o vacío
+    if(message === null || message === 0) {
+      // Si el mensaje es nulo o vacío, lanzo un TypeError
+      throw TypeError("Debes ingresar un mensaje y un desplazamiento");
+    }
+
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJ"
+    // Creo una cadena vacía para almacenar el mensaje cifrado
+    let encryptedString = "";
+    // Convierto el desplazamiento a un número entero
+    displacement = parseInt(displacement);
+
+    // Itero por cada letra en el mensaje
+    let i = 0;
+    while (i< message.length) {
+      // Obtengo la letra actual
+      const initialLetter = message[i];
+      // Obtengo la posición de la letra actual en el alfabeto
+      const initialLetterPosition = alphabet.indexOf(initialLetter);
+        
+      if(initialLetterPosition === -1){encryptedString = encryptedString + initialLetter
+      } else{
+        // Si la letra actual se encuentra en el alfabeto, calculo su nueva posición en el alfabeto
+        const finalLetterPosition = initialLetterPosition + displacement;
+        const newLetter = alphabet[finalLetterPosition];
+        // Agrego la nueva letra a encryptedString
+        encryptedString = encryptedString + newLetter
+      }
+      i++;
+    }
+    return encryptedString;
+  },
+
+  decode: function (encryptedMessage, displacement) { 
+    if(encryptedMessage === null || encryptedMessage === 0 ) {
+      throw TypeError("Debes ingresar un mensaje y un desplazamiento");
+    }
+    //                                                                    52
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Cree una cadena vacía para almacenar la cadena descifrada
+    let decryptedString = "";
+    // Itero por cada letra en la cadena cifrada
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      // Obtengo la letra cifrada actual
+      const currentLetter = encryptedMessage[i];
+      // Obtenemos la posición de la letra actual en el alfabeto
+      let currentLetterPosition = alphabet.indexOf(currentLetter);
+      
+      // Si la letra actual no se encuentra en el alfabeto, la agrego a decryptedString sin cifrarla
+      if (currentLetterPosition === -1) {
+        decryptedString = decryptedString + currentLetter;
+      } else {
+        // Si la letra actual se encuentra en el alfabeto, calculo su nueva posición en el alfabeto
+        currentLetterPosition = currentLetterPosition + 52
+        const newLetter = alphabet[currentLetterPosition - displacement];
+        // Agregamos la nueva letra a decryptedString
+        decryptedString = decryptedString + newLetter;
+      }
+    } 
+    // Devuelvo la cadena descifrada
+    return decryptedString;
+  }
+
 };
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,73 +1,38 @@
-import cipher from './cipher.js';
+import {decode, encode} from "./cipher"
 
 // declaracion de funciones
 function onSubmit(e){        
-    e.preventDefault()
-    let user = document.getElementById("user").value;
-    let password = document.getElementById("password").value;
-    let passwordStored = USERS[user];
+  e.preventDefault()
+  const user = document.getElementById("user").value;
+  const password = document.getElementById("password").value;
+  const passwordStored = USERS[user];
 
-    if( passwordStored ==  password) {
-        alert(" Tú sesión ha sido iniciada")
-        document.getElementById("welcome").style.display = 'none';
-        document.getElementById("data").style.display = 'none';
-    } else {
-        alert(" Usuario y/o contraseña incorrecta. Intenta de nuevo")
-    }
+  if( passwordStored ===  password) {
+    alert(" Tú sesión ha sido iniciada")
+    document.getElementById("welcome").style.display = 'none';
+    document.getElementById("data").style.display = 'none';
+  } else {
+    alert(" Usuario y/o contraseña incorrecta. Intenta de nuevo")
+  }
 }
 
-function cifrar(mensaje, desplazamiento){
-    const abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  
-    let cadenaCifrada = "";
-    desplazamiento = parseInt(desplazamiento);
-    
-    let t = 0;
-    while (t< mensaje.length) {
-        const letraInicial = mensaje[t];
-        const posicionLetraInicial = abecedario.indexOf(letraInicial);
-        
-        if(posicionLetraInicial == -1){
-            cadenaCifrada = cadenaCifrada + letraInicial 
-        } else{
-            const posicionLetraFinal = posicionLetraInicial + desplazamiento;
-            const nuevaLetra = abecedario[posicionLetraFinal];
-            cadenaCifrada = cadenaCifrada + nuevaLetra
-        }
-        t++;
-    }
-    
-    return cadenaCifrada;
-  }
-  
-  
-  function descifrar(text, displacements){
-    if(!text) {
-        return "";
-    }
-  
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    displacements = (displacements % 26 - 26) %26;
-    return text.replace(/[A-Z]/ig, c=> letters[(letters.indexOf(c)-displacements)%26]);
-    
-  }
 
 function startEncryptionEvents(){    
-    document.getElementById("mensajeaCifrar").addEventListener("keyup", function(){
-        this.value = this.value.toUpperCase();
-    });
-    document.getElementById("cifrar").addEventListener("click", function(){
-        let text = document.getElementById("mensajeaCifrar").value;
-        let displacements = document.getElementById("displacements").value;
-        document.getElementById("mensajeCifrado").value = cifrar(text, displacements)
-        document.getElementById('descifrar').disabled = false;
-    });
-    document.getElementById("descifrar").addEventListener("click", function(){
-        let text = document.getElementById("mensajeCifrado").value;
-        let displacements = document.getElementById("displacements").value;
-        document.getElementById("mensajeCifrado").value = descifrar(text, displacements )
-        document.getElementById('descifrar').disabled = true;
-    });
+  document.getElementById("mensajeaCifrar").addEventListener("keyup", function(){
+    this.value = this.value.toUpperCase();
+  });
+  document.getElementById("cifrar").addEventListener("click", function(){
+    const text = document.getElementById("mensajeaCifrar").value;
+    const displacements = document.getElementById("displacements").value;
+    document.getElementById("mensajeCifrado").value = decode(text, displacements)
+    document.getElementById('descifrar').disabled = false;
+  });
+  document.getElementById("descifrar").addEventListener("click", function(){
+    const text = document.getElementById("mensajeCifrado").value;
+    const displacements = document.getElementById("displacements").value;
+    document.getElementById("mensajeCifrado").value = encode(text, displacements )
+    document.getElementById('descifrar').disabled = true;
+  });
 }
 
 
@@ -76,13 +41,13 @@ function startEncryptionEvents(){
 //LOGIN DE INGRESO
 
 // JSON con "clave" : "valor"
-let USERS = {
-    "julian": "pass1",
-    "ivonnefm03": "pass2",
-    "antonella": "pass3",
+const USERS = {
+  "julian": "pass1",
+  "ivonnefm03": "pass2",
+  "antonella": "pass3",
 }
 
-let form = document.getElementById('login');
+const form = document.getElementById('login');
 form.addEventListener("submit", onSubmit);
 
 
